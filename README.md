d3ac9ad29c1fe7ec37cbba2cf222c551574289dd
Kadoya <aoi.kadoya@inesxi-eu-lx104.intra.rakuten.co.jp>
2019-03-01 22:16:16 -0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # nagios-msteams
-Nagios notification for MS teams
+A Nagios plugin to send notifications to MS teams
+
+## prerequisites
+
+These perl modules need to be installed.
+
+ - HTTP::Request
+ - LWP::UserAgent
+ - JSON
+
+## usage
+
+`--webhook` is required option. 
+`--nagios_url` to add a link in the notification.
+
+```
+export NAGIOS_HOSTALIAS="hoge101" NAGIOS_SERVICEDESC="http" NAGIOS_SERVICESTATE="WARNING" NAGIOS_SERVICEOUTPUT="test output"
+./nagios-msteams.pl --webhook 'https://your incoming webhook url'
+```
+
+## installation
+
+1. place the script in nagios plugin directory
+2. `chmod +x <path to Nagios plugin>/nagios-msteams.pl`
+3. configure commands, contacts
+
+
+# Reference
+
+https://docs.microsoft.com/en-us/outlook/actionable-messages/message-card-reference?ranMID=24542&ranEAID=je6NUbpObpQ&ranSiteID=je6NUbpObpQ-M2yBpYvoiCsKiucg39ve7Q
---
+++ nagios-msteams.pl
@@ -0,0 +1,111 @@
+#!/usr/bin/perl
+
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+use warnings;
+use strict;
+
+use Getopt::Long;
+use HTTP::Request::Common qw(POST);
+use HTTP::Status qw(is_client_error);
+use LWP::UserAgent;
+use JSON;
+
+my %event;
+my %nagios;
+my @sections;
+my @actions;
+my @targets;
+my $webhook = "https://outlook.office365.com/webhook/MISSING";
+my $nagios_url = "";
+my %color = ( 'OK' => '008000', 'WARNING' => 'ffff00', 'UNKNOWN' => '808080','CRITICAL' => 'ff0000',
+              'UP' => '008000', 'DOWN' => 'ff0000', 'UNREACHABLE' => 'ff8700');
+$event{'title'} = "Nagios Notification";
+$event{'@type'} = "MessageCard";
+$event{'@context'} = "https://schema.org/extensions";
+$nagios{'HOSTNOTES'} = "not defined";
+
+
+#
+# Get command-line options
+#
+GetOptions ("webhook=s" => \$webhook, "nagios_url=s" => \$nagios_url )
+or die("Error in command line arguments\n");
+
+#
+# Read Nagios events
+#
+while ((my $k, my $v) = each %ENV) {
+    next unless $k =~ /^(?:NAGIOS|ICINGA)_(.*)$/;
+    $nagios{$1} = $v;
+    print STDERR "$1 is $nagios{$1}\n";
+}
+
+#
+# Format message card
+#
+if (not defined $nagios{'SERVICESTATE'}) {
+    # if service state env is not set, event is host notification.
+    $nagios{'SERVICEDESC'} = "host status";
+    $nagios{'SERVICESTATE'} = $nagios{'HOSTSTATE'};
+    $nagios{'SERVICEOUTPUT'} = $nagios{'HOSTOUTPUT'};
+}
+$event{'themeColor'} = $color{"$nagios{'SERVICESTATE'}"};
+$event{'title'} = "$nagios{'HOSTALIAS'}/$nagios{'SERVICEDESC'} is $nagios{'SERVICESTATE'}";
+$event{'summary'} = $event{'title'};
+my @facts = ({
+    'name' => "Host:",
+    'value' => "$nagios{'HOSTALIAS'}"
+   },{
+    'name' => "Details:",
+    'value' => "$nagios{'SERVICEOUTPUT'}"
+});
+my %section = ( 'facts' => \@facts, 'text' => "Host Notes: $nagios{'HOSTNOTES'}" );
+push(@sections, \%section);
+$event{'sections'} = \@sections;
+
+if ($nagios_url ne '') {
+  my %target = (
+        'os' => 'default',
+        'uri' => "$nagios_url/cgi-bin/status.cgi?host=$nagios{'HOSTNAME'}"
+  );
+  push(@targets, \%target);
+  my %link = (
+      '@type' => 'OpenUri',
+      'name' => 'View in Nagios',
+      'targets' => \@targets
+  );
+  push(@actions, \%link);
+  $event{'potentialAction'} = \@actions;
+}
+my $json = encode_json \%event;
+
+
+#
+# Make the request
+#
+
+my $ua = LWP::UserAgent->new;
+$ua->timeout(15);
+
+my $req = HTTP::Request->new('POST', $webhook);
+$req->header('Content-Type' => 'application/json');
+$req->content($json);
+
+my $s = $req->as_string;
+print STDERR "Request:\n$s\n";
+
+my $resp = $ua->request($req);
+$s = $resp->as_string;
+print STDERR "Response:\n$s\n";
